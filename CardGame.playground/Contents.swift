aa04fffcc2a6a16e897064d113c80a3297a1ac05
Christian Lorenzo <clbmiami2004@gmail.com>
2019-09-24 22:17:32 -0700

    Added a comment to the code.

--- CardGame.playground/Contents.swift
+++ CardGame.playground/Contents.swift
@@ -142,6 +142,7 @@ struct Deck {
 //: Add a method to the deck called `drawCard()`. It takes no arguments and it returns a `Card` object. Have it draw a random card from the deck of cards and return it.
 //: - Callout(Hint): There should be `52` cards in the deck. So what if you created a random number within those bounds and then retrieved that card from the deck? Remember that arrays are indexed from `0` and take that into account with your random number picking.
 
+//This code under construction
 protocol GeneratesRandomNumbers {
     func random() -> Int
 }
