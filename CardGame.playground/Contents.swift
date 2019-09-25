b2db1c6a77c434ddfed056f57467b128b57b7a64
CrystalKnightCodes <CrystalKnightCodes@gmail.com>
2019-09-25 18:24:40 -0400

    Cleaned up code placement and formatting.

--- CardGame.playground/Contents.swift
+++ CardGame.playground/Contents.swift
@@ -23,6 +23,15 @@ enum Rank: Int {
 // Test line to make sure the static let works
 Rank.allRanks
 
+
+//: ## Step 2
+//: Once you've defined the enum as described above, take a look at this built-in protocol, [CustomStringConvertible](https://developer.apple.com/documentation/swift/customstringconvertible) and make the enum conform to that protocol. Make the face cards return a string of their name, and for the numbered cards, simply have it return that number as a string.
+//: ## Step 7
+//: In the rank enum, add a static computed property that returns all the ranks in an array. Name this property `allRanks`. This is needed because you can't iterate over all cases from an enum automatically.
+//: ## Step 16
+//: Take a look at the Swift docs for the [Comparable](https://developer.apple.com/documentation/swift/comparable) protocol. In particular, look at the two functions called `<` and `==`.
+//: ## Step 17
+//: Make the `Rank` type conform to the `Comparable` protocol. Implement the `<` and `==` functions such that they compare the `rawValue` of the `lhs` and `rhs` arguments passed in. This will allow us to compare two rank values with each other and determine whether they are equal, or if not, which one is larger.
 extension Rank: CustomStringConvertible {
     var description: String {
         switch self.rawValue {
@@ -40,6 +49,10 @@ extension Rank: CustomStringConvertible {
     }
 }
 
+//: ## Step 3
+//: Create an enum for the suit of a playing card. The values are `hearts`, `diamonds`, `spades`, and `clubs`. Use a raw type of `String` for this enum (this will allow us to get a string version of the enum cases for free, no use of `CustomStringConvertible` required).
+//: ## Step 8
+//: In the suit enum, add a static computed property that returns all the suits in an array. Name this property `allSuits`.
 enum Suit: String {
     case hearts = "Hearts"
     case diamonds = "Diamonds"
@@ -51,6 +64,12 @@ enum Suit: String {
 // Test line to make sure static let works as intended
 Suit.allSuits
 
+//: ## Step 4
+//: Using the two enums above, create a `struct` called `Card` to model a single playing card. It should have constant properties for each constituent piece (one for suit and one for rank).
+//: ## Step 5
+//: Make the card also conform to `CustomStringConvertible`. When turned into a string, a card's value should look something like this, "ace of spades", or "3 of diamonds".
+//: Step 18
+//: Make the `Card` type conform to the `Comparable` protocol. Implement the `<` and `==` methods such that they compare the ranks of the `lhs` and `rhs` arguments passed in. For the `==` method, compare **both** the rank and the suit.
 struct Card: CustomStringConvertible {
     var description: String {
         return "\(rank) of \(suit)"
@@ -59,56 +78,6 @@ struct Card: CustomStringConvertible {
     let suit: Suit
     let rank: Rank
 }
-
-struct Deck {
-let cards: [Card]
-
-init() {
-    var deck: [Card] = []
-    var currentCard: Card
-    for rank in Rank.allRanks {
-        for suit in Suit.allSuits {
-            currentCard = Card.init(suit: suit, rank: rank)
-           deck.append(currentCard)
-        }
-    }
-    self.cards = deck
-    
-    func drawCard() -> Card {
-        let randomCard = Int.random(in: 1...52)
-        return cards[randomCard + 1]
-}
-
-
-
-//: ## Step 2
-//: Once you've defined the enum as described above, take a look at this built-in protocol, [CustomStringConvertible](https://developer.apple.com/documentation/swift/customstringconvertible) and make the enum conform to that protocol. Make the face cards return a string of their name, and for the numbered cards, simply have it return that number as a string.
-
-
-//: ## Step 7
-//: In the rank enum, add a static computed property that returns all the ranks in an array. Name this property `allRanks`. This is needed because you can't iterate over all cases from an enum automatically.
-//: ## Step 16
-//: Take a look at the Swift docs for the [Comparable](https://developer.apple.com/documentation/swift/comparable) protocol. In particular, look at the two functions called `<` and `==`.
-//: ## Step 17
-//: Make the `Rank` type conform to the `Comparable` protocol. Implement the `<` and `==` functions such that they compare the `rawValue` of the `lhs` and `rhs` arguments passed in. This will allow us to compare two rank values with each other and determine whether they are equal, or if not, which one is larger.
-
-
-
-//: ## Step 3
-//: Create an enum for the suit of a playing card. The values are `hearts`, `diamonds`, `spades`, and `clubs`. Use a raw type of `String` for this enum (this will allow us to get a string version of the enum cases for free, no use of `CustomStringConvertible` required).
-//: ## Step 8
-//: In the suit enum, add a static computed property that returns all the suits in an array. Name this property `allSuits`.
-
-
-
-//: ## Step 4
-//: Using the two enums above, create a `struct` called `Card` to model a single playing card. It should have constant properties for each constituent piece (one for suit and one for rank).
-//: ## Step 5
-//: Make the card also conform to `CustomStringConvertible`. When turned into a string, a card's value should look something like this, "ace of spades", or "3 of diamonds".
-//: Step 18
-//: Make the `Card` type conform to the `Comparable` protocol. Implement the `<` and `==` methods such that they compare the ranks of the `lhs` and `rhs` arguments passed in. For the `==` method, compare **both** the rank and the suit.
-
-
 
 //: ## Step 6
 //: Create a `struct` to model a deck of cards. It should be called `Deck` and have an array of `Card` objects as a constant property. A custom `init` function should be created that initializes the array with a card of each rank and suit. You'll want to iterate over all ranks, and then over all suits (this is an example of _nested `for` loops_). See the next 2 steps before you continue with the nested loops.
@@ -124,8 +93,25 @@ init() {
 //: ## Step 11
 //: Add a method to the deck called `drawCard()`. It takes no arguments and it returns a `Card` object. Have it draw a random card from the deck of cards and return it.
 //: - Callout(Hint): There should be `52` cards in the deck. So what if you created a random number within those bounds and then retrieved that card from the deck? Remember that arrays are indexed from `0` and take that into account with your random number picking.
+struct Deck {
+let cards: [Card]
 
-
+init() {
+    var deck: [Card] = []
+    var currentCard: Card
+    for rank in Rank.allRanks {
+        for suit in Suit.allSuits {
+            currentCard = Card.init(suit: suit, rank: rank)
+           deck.append(currentCard)
+            }
+        }
+    self.cards = deck
+    }
+    func drawCard() -> Card {
+        let randomCard = Int.random(in: 1...52)
+        return cards[randomCard + 1]
+    }
+}
 
 //: ## Step 12
 //: Create a protocol for a `CardGame`. It should have two requirements:
