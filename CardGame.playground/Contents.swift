c616b6ae945096be68d39c7ff578e8fff963b82d
Pioneer Pass <pioneerpass@protonmail.com>
2019-10-09 15:41:02 -0700

    Ignored the directions completely. The game works, though! <3

--- CardGame.playground/Contents.swift
+++ CardGame.playground/Contents.swift
@@ -1,90 +1,144 @@
-import Foundation
+import UIKit
 
-//: ## Step 1
-//: Create an enumeration for the value of a playing card. The values are: `ace`, `two`, `three`, `four`, `five`, `six`, `seven`, `eight`, `nine`, `ten`, `jack`, `queen`, and `king`. Set the raw type of the enum to `Int` and assign the ace a value of `1`.
-//: ## Step 2
-//: Once you've defined the enum as described above, take a look at this built-in protocol, [CustomStringConvertible](https://developer.apple.com/documentation/swift/customstringconvertible) and make the enum conform to that protocol. Make the face cards return a string of their name, and for the numbered cards, simply have it return that number as a string.
-//: ## Step 7
-//: In the rank enum, add a static computed property that returns all the ranks in an array. Name this property `allRanks`. This is needed because you can't iterate over all cases from an enum automatically.
-//: ## Step 16
-//: Take a look at the Swift docs for the [Comparable](https://developer.apple.com/documentation/swift/comparable) protocol. In particular, look at the two functions called `<` and `==`.
-//: ## Step 17
-//: Make the `Rank` type conform to the `Comparable` protocol. Implement the `<` and `==` functions such that they compare the `rawValue` of the `lhs` and `rhs` arguments passed in. This will allow us to compare two rank values with each other and determine whether they are equal, or if not, which one is larger.
+enum Rank: Int {
+    case ace = 1, one, two, three, four, five, six, seven, eight, nine, ten, jack, queen, king
+}
 
+var blah = Rank.ace.rawValue
 
+enum Suit: String {
+    case hearts
+    case diamonds
+    case spades
+    case clubs
+}
 
-//: ## Step 3
-//: Create an enum for the suit of a playing card. The values are `hearts`, `diamonds`, `spades`, and `clubs`. Use a raw type of `String` for this enum (this will allow us to get a string version of the enum cases for free, no use of `CustomStringConvertible` required).
-//: ## Step 8
-//: In the suit enum, add a static computed property that returns all the suits in an array. Name this property `allSuits`.
+struct Card {
+    var rank: Rank
+    var suit: Suit
+}
 
+var counter = 0
+var populate: Card
+var populate2: Card
+var deckArray = [Card]()
 
+populate = Card(rank: .ace, suit: .hearts)
+populate2 = Card(rank: .one, suit: .clubs)
 
-//: ## Step 4
-//: Using the two enums above, create a `struct` called `Card` to model a single playing card. It should have constant properties for each constituent piece (one for suit and one for rank).
-//: ## Step 5
-//: Make the card also conform to `CustomStringConvertible`. When turned into a string, a card's value should look something like this, "ace of spades", or "3 of diamonds".
-//: Step 18
-//: Make the `Card` type conform to the `Comparable` protocol. Implement the `<` and `==` methods such that they compare the ranks of the `lhs` and `rhs` arguments passed in. For the `==` method, compare **both** the rank and the suit.
+deckArray.append(populate2)
 
+while counter <= 50 {
+    if counter >= 0 && counter <= 12 {
+        
+        switch counter {
+        case 0, 13, 26, 39: populate.rank = .ace
+        case 1, 14, 27, 40: populate.rank = .two
+        case 2, 15, 28, 41: populate.rank = .three
+        case 3, 16, 29, 42: populate.rank = .four
+        case 4, 17, 30, 43: populate.rank = .five
+        case 5, 18, 31, 44: populate.rank = .six
+        case 6, 19, 32, 45: populate.rank = .seven
+        case 7, 20, 33, 46: populate.rank = .eight
+        case 8, 21, 34, 47: populate.rank = .nine
+        case 9, 22, 35, 48: populate.rank = .ten
+        case 10, 23, 36, 49: populate.rank = .jack
+        case 11, 24, 37, 50: populate.rank = .queen
+        case 12, 25, 38, 51: populate.rank = .king
+        default: populate.rank = .ace
+        }
+        
+        populate.suit = .clubs
+   
+        deckArray.append(populate) }
+        
+    else if counter >= 13 && counter <= 25 {
+        
+            populate.suit = .diamonds
+            switch counter {
+            case 0, 13, 26, 39: populate.rank = .ace
+            case 1, 14, 27, 40: populate.rank = .two
+            case 2, 15, 28, 41: populate.rank = .three
+            case 3, 16, 29, 42: populate.rank = .four
+            case 4, 17, 30, 43: populate.rank = .five
+            case 5, 18, 31, 44: populate.rank = .six
+            case 6, 19, 32, 45: populate.rank = .seven
+            case 7, 20, 33, 46: populate.rank = .eight
+            case 8, 21, 34, 47: populate.rank = .nine
+            case 9, 22, 35, 48: populate.rank = .ten
+            case 10, 23, 36, 49: populate.rank = .jack
+            case 11, 24, 37, 50: populate.rank = .queen
+            case 12, 25, 38, 51: populate.rank = .king
+            default: populate.rank = .ace
+            }
+            deckArray.append(populate) }
+        
+    else if counter >= 26 && counter <= 38 {
+            populate.suit = .hearts
+            switch counter {
+            case 0, 13, 26, 39: populate.rank = .ace
+            case 1, 14, 27, 40: populate.rank = .two
+            case 2, 15, 28, 41: populate.rank = .three
+            case 3, 16, 29, 42: populate.rank = .four
+            case 4, 17, 30, 43: populate.rank = .five
+            case 5, 18, 31, 44: populate.rank = .six
+            case 6, 19, 32, 45: populate.rank = .seven
+            case 7, 20, 33, 46: populate.rank = .eight
+            case 8, 21, 34, 47: populate.rank = .nine
+            case 9, 22, 35, 48: populate.rank = .ten
+            case 10, 23, 36, 49: populate.rank = .jack
+            case 11, 24, 37, 50: populate.rank = .queen
+            case 12, 25, 38, 51: populate.rank = .king
+            default: populate.rank = .ace
+            }
+            deckArray.append(populate) }
+        
+    else if counter >= 39 && counter <= 51 {
+            populate.suit = .spades
+            switch counter {
+            case 0, 13, 26, 39: populate.rank = .ace
+            case 1, 14, 27, 40: populate.rank = .two
+            case 2, 15, 28, 41: populate.rank = .three
+            case 3, 16, 29, 42: populate.rank = .four
+            case 4, 17, 30, 43: populate.rank = .five
+            case 5, 18, 31, 44: populate.rank = .six
+            case 6, 19, 32, 45: populate.rank = .seven
+            case 7, 20, 33, 46: populate.rank = .eight
+            case 8, 21, 34, 47: populate.rank = .nine
+            case 9, 22, 35, 48: populate.rank = .ten
+            case 10, 23, 36, 49: populate.rank = .jack
+            case 11, 24, 37, 50: populate.rank = .queen
+            case 12, 25, 38, 51: populate.rank = .king
+            default: populate.rank = .ace
+            }
+            deckArray.append(populate) }
 
+    counter += 1
+//        print("\(deckArray[counter].rank) of \(deckArray[counter].suit)")
+}
 
-//: ## Step 6
-//: Create a `struct` to model a deck of cards. It should be called `Deck` and have an array of `Card` objects as a constant property. A custom `init` function should be created that initializes the array with a card of each rank and suit. You'll want to iterate over all ranks, and then over all suits (this is an example of _nested `for` loops_). See the next 2 steps before you continue with the nested loops.
-//: ## Step 9
-//: Back to the `Deck` and the nested loops. Now that you have a way to get arrays of all rank values and all suit values, create 2 `for` loops in the `init` method, one nested inside the other, where you iterate over each value of rank, and then iterate over each value of suit. See an example below to get an idea of how this will work. Imagine an enum that contains the 4 cardinal directions, and imagine that enum has a property `allDirections` that returns an array of them.
-//: ```
-//: for direction in Compass.allDirections {
-//:
-//:}
-//:```
-//: ## Step 10
-//: These loops will allow you to match up every rank with every suit. Make a `Card` object from all these pairings and append each card to the `cards` property of the deck. At the end of the `init` method, the `cards` array should contain a full deck of standard playing card objects.
-//: ## Step 11
-//: Add a method to the deck called `drawCard()`. It takes no arguments and it returns a `Card` object. Have it draw a random card from the deck of cards and return it.
-//: - Callout(Hint): There should be `52` cards in the deck. So what if you created a random number within those bounds and then retrieved that card from the deck? Remember that arrays are indexed from `0` and take that into account with your random number picking.
+var playerOneRand = Int.random(in: 0...51)
+var playerTwoRand = Int.random(in: 0...51)
 
+//NOT THE SAME CARD
+while playerOneRand == playerTwoRand {
+    playerOneRand = Int.random(in: 0...51)
+    playerTwoRand = Int.random(in: 0...51)
+}
 
+var playerOneCard = deckArray[playerOneRand]
+print("Player One draws a \(playerOneCard.rank) of \(playerOneCard.suit.rawValue)")
 
-//: ## Step 12
-//: Create a protocol for a `CardGame`. It should have two requirements:
-//: * a gettable `deck` property
-//: * a `play()` method
+var playerTwoCard = deckArray[playerTwoRand]
+print("Player Two draws a \(playerTwoCard.rank) of \(playerTwoCard.suit.rawValue)")
+print("++++++++++++++++")
 
-
-
-//: ## Step 13
-//: Create a protocol for tracking a card game as a delegate called `CardGameDelegate`. It should have two functional requirements:
-//: * a function called `gameDidStart` that takes a `CardGame` as an argument
-//: * a function with the following signature: `game(player1DidDraw card1: Card, player2DidDraw card2: Card)`
-
-
-
-//: ## Step 14
-//: Create a class called `HighLow` that conforms to the `CardGame` protocol. It should have an initialized `Deck` as a property, as well as an optional delegate property of type `CardGameDelegate`.
-//: ## Step 15
-//: As part of the protocol conformance, implement a method called `play()`. The method should draw 2 cards from the deck, one for player 1 and one for player 2. These cards will then be compared to see which one is higher. The winning player will be printed along with a description of the winning card. Work will need to be done to the `Suit` and `Rank` types above, so see the next couple steps before continuing with this step.
-//: ## Step 19
-//: Back to the `play()` method. With the above types now conforming to `Comparable`, you can write logic to compare the drawn cards and print out 1 of 3 possible message types:
-//: * Ends in a tie, something like, "Round ends in a tie with 3 of clubs."
-//: * Player 1 wins with a higher card, e.g. "Player 1 wins with 8 of hearts."
-//: * Player 2 wins with a higher card, e.g. "Player 2 wins with king of diamonds."
-
-
-
-//: ## Step 20
-//: Create a class called `CardGameTracker` that conforms to the `CardGameDelegate` protocol. Implement the two required functions: `gameDidStart` and `game(player1DidDraw:player2DidDraw)`. Model `gameDidStart` after the same method in the guided project from today. As for the other method, have it print a message like the following:
-//: * "Player 1 drew a 6 of hearts, player 2 drew a jack of spades."
-
-
-
-//: Step 21
-//: Time to test all the types you've created. Create an instance of the `HighLow` class. Set the `delegate` property of that object to an instance of `CardGameTracker`. Lastly, call the `play()` method on the game object. It should print out to the console something that looks similar to the following:
-//:
-//: ```
-//: Started a new game of High Low
-//: Player 1 drew a 2 of diamonds, player 2 drew a ace of diamonds.
-//: Player 1 wins with 2 of diamonds.
-//: ```
+if playerOneCard.rank.rawValue > playerTwoCard.rank.rawValue {
+    print("Player One Wins")
+} else if playerOneCard.rank.rawValue < playerTwoCard.rank.rawValue {
+    print("Player Two Wins")
+} else if playerOneCard.rank.rawValue == playerTwoCard.rank.rawValue {
+    print("Tie.")
+}
 
 
