43e9313584adb4de1c766e35ecc052e65b70e3a8
Zack Larsen <zacklarsen1@gmail.com>
2019-11-06 16:51:00 -0700

    working on step 11

--- CardGame.playground/Contents.swift
+++ CardGame.playground/Contents.swift
@@ -83,22 +83,10 @@ extension Card: CustomStringConvertible {
 
 //: ## Step 6
 //: Create a `struct` to model a deck of cards. It should be called `Deck` and have an array of `Card` objects as a constant property. A custom `init` function should be created that initializes the array with a card of each rank and suit. You'll want to iterate over all ranks, and then over all suits (this is an example of _nested `for` loops_). See the next 2 steps before you continue with the nested loops.
-struct Deck {
-    let cards: [Card]
-
-    init() {
-        var cardStack: [Card] = []
-        for suit in Suits.allSuits {
-        for rank in Rank.allRanks {
-            cardDeck.append(Card(suit: suit, rank: rank))
-        }
-    }
-    }
-}
 
 //: ## Step 9
 //: Back to the `Deck` and the nested loops. Now that you have a way to get arrays of all rank values and all suit values, create 2 `for` loops in the `init` method, one nested inside the other, where you iterate over each value of rank, and then iterate over each value of suit. See an example below to get an idea of how this will work. Imagine an enum that contains the 4 cardinal directions, and imagine that enum has a property `allDirections` that returns an array of them.
-
+//above
 
 
 //: ```
@@ -108,6 +96,25 @@ struct Deck {
 //:```
 //: ## Step 10
 //: These loops will allow you to match up every rank with every suit. Make a `Card` object from all these pairings and append each card to the `cards` property of the deck. At the end of the `init` method, the `cards` array should contain a full deck of standard playing card objects.
+struct Deck {
+let cards: [Card]
+
+init() {
+    var cards: [Card] = []
+    for suit in Suits.allSuits {
+        for rank in Rank.allRanks {
+            let aCard = Card(suit: suit, rank: rank)
+            cards.append(aCard)
+        }
+    }
+    self.cards = cards
+}
+
+func drawCard() -> Card {
+    let randomNumber = Int.random(in: 1...cards.count) - 1
+    return cards[randomNumber]
+}
+
 
 
 
