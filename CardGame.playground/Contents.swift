234eed5521b02cf39d3c5854d5bf74041e146ed1
Claudia Maciel <clc_80@yahoo.com>
2020-02-03 21:31:03 -0800

    Tested steps

--- CardGame.playground/Contents.swift
+++ CardGame.playground/Contents.swift
@@ -2,7 +2,7 @@ import Foundation
 
 //: ## Step 1
 //: Create an enumeration for the value of a playing card. The values are: `ace`, `two`, `three`, `four`, `five`, `six`, `seven`, `eight`, `nine`, `ten`, `jack`, `queen`, and `king`. Set the raw type of the enum to `Int` and assign the ace a value of `1`.
-enum playingCard: Int {
+enum PlayingCard: Int {
     case ace = 1
     case two
     case three
@@ -19,7 +19,7 @@ enum playingCard: Int {
 }
 //: ## Step 2
 //: Once you've defined the enum as described above, take a look at this built-in protocol, [CustomStringConvertible](https://developer.apple.com/documentation/swift/customstringconvertible) and make the enum conform to that protocol. Make the face cards return a string of their name, and for the numbered cards, simply have it return that number as a string.
-extension playingCard: CustomStringConvertible {
+extension PlayingCard: CustomStringConvertible {
     var description: String {
         var cardValue = ""
         
@@ -41,6 +41,8 @@ extension playingCard: CustomStringConvertible {
     }
 }
 
+let newCard = PlayingCard.eigth
+print(newCard.description)
 //: ## Step 3
 //: Create an enum for the suit of a playing card. The values are `hearts`, `diamonds`, `spades`, and `clubs`. Use a raw type of `String` for this enum (this will allow us to get a string version of the enum cases for free, no use of `CustomStringConvertible` required).
 
