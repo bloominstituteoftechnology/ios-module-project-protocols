30bf8dc8d19ffa1e84c63202953cbc826f02e9c5
Libby Thomas <thomaslibby05@gmail.com>
2020-02-04 21:05:17 -0600

    Finished Step 11

--- CardGame.playground/Contents.swift
+++ CardGame.playground/Contents.swift
@@ -118,12 +118,13 @@ struct Deck {
 //:}
 //:```
 
+// done
 
 
 //: ## Step 10
 //: These loops will allow you to match up every rank with every suit. Make a `Card` object from all these pairings and append each card to the `cards` property of the deck. At the end of the `init` method, the `cards` array should contain a full deck of standard playing card objects.
 
-
+// done
 
 
 
@@ -131,7 +132,12 @@ struct Deck {
 //: Add a method to the deck called `drawCard()`. It takes no arguments and it returns a `Card` object. Have it draw a random card from the deck of cards and return it.
 //: - Callout(Hint): There should be `52` cards in the deck. So what if you created a random number within those bounds and then retrieved that card from the deck? Remember that arrays are indexed from `0` and take that into account with your random number picking.
 
-
+extension Deck {
+    func drawCard() -> Card {
+        let randomCard = Int.random(in: 0...cards.count)
+        return cards[randomCard]
+    }
+}
 
 
 
