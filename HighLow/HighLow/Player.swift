a03112f01bba8f9aa8867657ef819f49bdcc4495
Shawn Gee <shawngee@gmail.com>
2020-02-19 16:12:44 -0500

    Basic functionality

---
+++ HighLow/HighLow/Player.swift
@@ -0,0 +1,19 @@
+//
+//  Player.swift
+//  HighLow
+//
+//  Created by Shawn Gee on 2/19/20.
+//  Copyright © 2020 Swift Student. All rights reserved.
+//
+
+import Foundation
+
+class Player {
+    let name: String
+    var score = 0
+    var bet: HighLowBlitz.Bet?
+    
+    init(name: String) {
+        self.name = name
+    }
+}
