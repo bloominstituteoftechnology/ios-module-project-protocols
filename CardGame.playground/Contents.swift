8a144438ddb6a27a28ff086a891617850187d195
Shawn-James <shawn.james@me.com>
2020-02-19 18:06:14 -0500

    up to step 5 completed

--- CardGame.playground/Contents.swift
+++ CardGame.playground/Contents.swift
@@ -2,7 +2,7 @@ import Foundation
 
 //: ## Step 1
 //: Create an enumeration for the value of a playing card. The values are: `ace`, `two`, `three`, `four`, `five`, `six`, `seven`, `eight`, `nine`, `ten`, `jack`, `queen`, and `king`. Set the raw type of the enum to `Int` and assign the ace a value of `1`.
-enum CardValue: Int {
+enum Rank: Int {
     case ace = 1
     case two = 2
     case three = 3
@@ -19,7 +19,7 @@ enum CardValue: Int {
 }
 //: ## Step 2
 //: Once you've defined the enum as described above, take a look at this built-in protocol, [CustomStringConvertible](https://developer.apple.com/documentation/swift/customstringconvertible) and make the enum conform to that protocol. Make the face cards return a string of their name, and for the numbered cards, simply have it return that number as a string.
-extension CardValue: CustomStringConvertible {
+extension Rank: CustomStringConvertible {
     var description: String {
         switch self {
         case .ace:
@@ -47,18 +47,27 @@ enum CardSuit: String {
 }
 //: ## Step 4
 //: Using the two enums above, create a `struct` called `Card` to model a single playing card. It should have constant properties for each constituent piece (one for suit and one for rank).
-
-
-
-
+struct Card {
+    let rank: Rank
+    let suit: CardSuit
+}
 //: ## Step 5
 //: Make the card also conform to `CustomStringConvertible`. When turned into a string, a card's value should look something like this, "ace of spades", or "3 of diamonds".
-
-
-
+extension Card: CustomStringConvertible {
+    var description: String {
+        return rank.description + " of " + suit.rawValue
+    }
+    
+}
 //: ## Step 6
 //: Create a `struct` to model a deck of cards. It should be called `Deck` and have an array of `Card` objects as a constant property. A custom `init` function should be created that initializes the array with a card of each rank and suit. You'll want to iterate over all ranks, and then over all suits (this is an example of _nested `for` loops_). See the next 2 steps before you continue with the nested loops.
-
+//struct Deck {
+//    var cardArray: [Card]
+//    
+//    init(<#parameters#>) {
+//        <#statements#>
+//    }
+//}
 
 
 
