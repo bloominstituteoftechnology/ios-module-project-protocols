cad6707f31272e9bb26eeefce3d44d83fa0df40c
Bharat Kumar <Bharatiosdeveloper@gmail.com>
2020-02-19 19:03:07 -0800

    created protocol for CardGame

--- CardGame.playground/Contents.swift
+++ CardGame.playground/Contents.swift
@@ -166,13 +166,13 @@ struct Deck {
 //: Create a protocol for a `CardGame`. It should have two requirements:
 //: * a gettable `deck` property
 //: * a `play()` method
-//protocol CardGame {
-//    var getDeck: Deck { get }
-//
-//    func play()
-//
-//
-//}
+protocol CardGame {
+    var getDeck: Deck { get }
+
+    func play()
+
+
+}
 
 
 
