528739670c6df8d5c5c9621dd4a7faf47153393c
Bharat Kumar <Bharatiosdeveloper@gmail.com>
2020-02-19 19:03:59 -0800

    Created protocol for CardGameDelegate to add two functions

--- CardGame.playground/Contents.swift
+++ CardGame.playground/Contents.swift
@@ -180,11 +180,11 @@ protocol CardGame {
 //: Create a protocol for tracking a card game as a delegate called `CardGameDelegate`. It should have two functional requirements:
 //: * a function called `gameDidStart` that takes a `CardGame` as an argument
 //: * a function with the following signature: `game(player1DidDraw card1: Card, player2DidDraw card2: Card)`
-//protocol CardGameDelegate {
-//    func gameDidStart(game: CardGame)
-//    func game(player1DidDraw card1: Card, player2DidDraw card2: Card)
-//
-//}
+protocol CardGameDelegate {
+    func gameDidStart(game: CardGame)
+    func game(player1DidDraw card1: Card, player2DidDraw card2: Card)
+
+}
 
 
 
