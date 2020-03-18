b366790357d04b57b96806328326b4ab65e41d23
Ronald Huston Jr <rghuston9@gmail.com>
2020-03-18 16:42:47 -0500

    step 5

--- CardGame.playground/Contents.swift
+++ CardGame.playground/Contents.swift
@@ -2,7 +2,7 @@ import Foundation
 
 //: ## Step 1
 //: Create an enumeration for the value of a playing card. The values are: `ace`, `two`, `three`, `four`, `five`, `six`, `seven`, `eight`, `nine`, `ten`, `jack`, `queen`, and `king`. Set the raw type of the enum to `Int` and assign the ace a value of `1`.
-enum Card: Int {
+enum PlayingCard: Int {
     case ace = 1
     case two
     case three
@@ -22,7 +22,7 @@ enum Card: Int {
 
 //: ## Step 2
 //: Once you've defined the enum as described above, take a look at this built-in protocol, [CustomStringConvertible](https://developer.apple.com/documentation/swift/customstringconvertible) and make the enum conform to that protocol. Make the face cards return a string of their name, and for the numbered cards, simply have it return that number as a string.
-extension Card: CustomStringConvertible {
+extension PlayingCard: CustomStringConvertible {
     var description: String {
         switch self {
         case .two:
@@ -58,19 +58,27 @@ extension Card: CustomStringConvertible {
 
 //: ## Step 3
 //: Create an enum for the suit of a playing card. The values are `hearts`, `diamonds`, `spades`, and `clubs`. Use a raw type of `String` for this enum (this will allow us to get a string version of the enum cases for free, no use of `CustomStringConvertible` required).
-
-
-
+enum Suit: String {
+    case hearts = "hearts"
+    case spade = "spades"
+    case diamonds = "diamonds"
+    case clubs = "clubs"
+}
 
 //: ## Step 4
 //: Using the two enums above, create a `struct` called `Card` to model a single playing card. It should have constant properties for each constituent piece (one for suit and one for rank).
-
-
-
+struct Card {
+    let suit: Suit
+    let rank: PlayingCard
+}
 
 //: ## Step 5
 //: Make the card also conform to `CustomStringConvertible`. When turned into a string, a card's value should look something like this, "ace of spades", or "3 of diamonds".
-
+extension Card: CustomStringConvertible {
+    var description: String {
+        return "\(self.rank) of \(self.suit)"
+    }
+}
 
 
 //: ## Step 6
