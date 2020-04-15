6c3cd9fe2e9726500b2936e429e763465efcd7bd
Ronald Huston Jr <rghuston9@gmail.com>
2020-04-15 15:22:53 -0500

    thru step 5

--- CardGame.playground/Contents.swift
+++ CardGame.playground/Contents.swift
@@ -61,20 +61,25 @@ enum Suit: String {
     case hearts
     case diamonds
     case spades
-    case clubs 
+    case clubs
 }
 
 
 
 //: ## Step 4
 //: Using the two enums above, create a `struct` called `Card` to model a single playing card. It should have constant properties for each constituent piece (one for suit and one for rank).
-
-
-
+struct Card {
+    let suit: Suit
+    let rank: PlayingCard
+}
 
 //: ## Step 5
 //: Make the card also conform to `CustomStringConvertible`. When turned into a string, a card's value should look something like this, "ace of spades", or "3 of diamonds".
-
+extension Card: CustomStringConvertible {
+    var description: String {
+        return "\(self.rank) of \(self.suit)"
+    }
+}
 
 
 //: ## Step 6
