fd43c1138ca12b41de2374b6589d472af2f094df
violet-love <violetlove9419@icloud.com>
2020-04-16 16:13:48 -0700

    made some progress, still struggling with CustomStringConvertible Protocol

--- CardGame.playground/Contents.swift
+++ CardGame.playground/Contents.swift
@@ -52,22 +52,35 @@ extension Rank: CustomStringConvertible {
             return "King"
         default:
             print("Uh-Oh it's broken")
+        }
 
 //: ## Step 3
 //: Create an enum for the suit of a playing card. The values are `hearts`, `diamonds`, `spades`, and `clubs`. Use a raw type of `String` for this enum (this will allow us to get a string version of the enum cases for free, no use of `CustomStringConvertible` required).
-
+            enum Suit: String {
+                case spades = "Spades"
+                case hearts = "Hearts"
+                case diamonds = "Diamonds"
+                case clubs = "Clubs"
+}
 
 
 
 //: ## Step 4
 //: Using the two enums above, create a `struct` called `Card` to model a single playing card. It should have constant properties for each constituent piece (one for suit and one for rank).
-
+            struct Card {
+                let rank: Rank
+                let suit: Suit
+}
 
 
 
 //: ## Step 5
 //: Make the card also conform to `CustomStringConvertible`. When turned into a string, a card's value should look something like this, "ace of spades", or "3 of diamonds".
-
+extension Card: CustomStringConvertible {
+                var Description: String {
+                    return "\(Rank.self.RawValue) of \(Suit.self.RawValue)"
+                }
+}
 
 
 //: ## Step 6
