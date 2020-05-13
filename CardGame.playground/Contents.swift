977daca963f3f8e8787a999b932633c22d305c75
violet-love <violetlove9419@icloud.com>
2020-05-13 14:36:15 -0700

    created a playing card enum and made it conform to custom string convertible

--- CardGame.playground/Contents.swift
+++ CardGame.playground/Contents.swift
@@ -2,14 +2,60 @@ import Foundation
 
 //: ## Step 1
 //: Create an enumeration for the value of a playing card. The values are: `ace`, `two`, `three`, `four`, `five`, `six`, `seven`, `eight`, `nine`, `ten`, `jack`, `queen`, and `king`. Set the raw type of the enum to `Int` and assign the ace a value of `1`.
-
+enum PlayingCard: Int {
+    
+    case ace = 1
+    case two = 2
+    case three = 3
+    case four = 4
+    case five = 5
+    case six = 6
+    case seven = 7
+    case eight = 8
+    case nine = 9
+    case ten = 10
+    case jack = 11
+    case queen = 12
+    case king = 13
+    
+}
 
 
 
 
 //: ## Step 2
 //: Once you've defined the enum as described above, take a look at this built-in protocol, [CustomStringConvertible](https://developer.apple.com/documentation/swift/customstringconvertible) and make the enum conform to that protocol. Make the face cards return a string of their name, and for the numbered cards, simply have it return that number as a string.
-
+extension PlayingCard: CustomStringConvertible {
+    var description: String {
+        switch self {
+        case .ace:
+            return "Ace"
+        case .two:
+            return "Two"
+        case .three:
+            return "Three"
+        case .four:
+            return "Four"
+        case .five:
+            return "Five"
+        case .six:
+            return "Six"
+        case .seven:
+            return "Seven"
+        case .eight:
+            return "Eight"
+        case .nine:
+            return "Nine"
+        case .ten:
+            return "Ten"
+        case .jack:
+            return "Jack"
+        case .queen:
+            return "Queen"
+        case .king:
+            return "King"
+    }
+}
 
 
 
