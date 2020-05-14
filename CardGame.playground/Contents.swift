30d667ba0e3337e1fb201594a9773d250daded0a
Ian Becker <ian-becker@lambdastudents.com>
2020-05-13 18:55:17 -0700

    Added a method to draw a random card, made a CardGame protocol, and CardGameDelegate

--- CardGame.playground/Contents.swift
+++ CardGame.playground/Contents.swift
@@ -21,8 +21,6 @@ enum Rank: Int {
         return [.ace, .two, .three, .four, .five, .six, .seven, .eight,. nine, .ten, .jack, .queen, .king]
     }
 }
-
-
 //: ## Step 2
 //: Once you've defined the enum as described above, take a look at this built-in protocol, [CustomStringConvertible](https://developer.apple.com/documentation/swift/customstringconvertible) and make the enum conform to that protocol. Make the face cards return a string of their name, and for the numbered cards, simply have it return that number as a string.
 extension Rank: CustomStringConvertible {
@@ -102,22 +100,21 @@ struct Deck {
         }
         self.cardDeck = cardArray
     }
+    func drawCard() -> Card {
+        return (cardDeck[Int.random(in: 0...51)])
+    }
 }
 
-// create 2 for loops in the init method, one nested inside the other, where you iterate over each value of rank, and then iterate over each value of suit.
+// create 2 for loops in the init method, one nested inside the other, where you iterate over each value of rank, and then iterate over each value of suit
 
-
+// Add a method to the deck called drawCard(). It takes no arguments and it returns a Card object. Have it draw a random card from the deck of cards and return it.
+//
+//There should be 52 cards in the deck. So what if you created a random number within those bounds and then retrieved that card from the deck? Remember that arrays are indexed from 0 and take that into account with your random number picking.
 //: ## Step 7
 //: In the rank enum, add a static computed property that returns all the ranks in an array. Name this property `allRanks`. This is needed because you can't iterate over all cases from an enum automatically.
 
-
-
-
 //: ## Step 8
 //: In the suit enum, add a static computed property that returns all the suits in an array. Name this property `allSuits`.
-
-
-
 
 //: ## Step 9
 //: Back to the `Deck` and the nested loops. Now that you have a way to get arrays of all rank values and all suit values, create 2 `for` loops in the `init` method, one nested inside the other, where you iterate over each value of rank, and then iterate over each value of suit. See an example below to get an idea of how this will work. Imagine an enum that contains the 4 cardinal directions, and imagine that enum has a property `allDirections` that returns an array of them.
@@ -127,39 +124,32 @@ struct Deck {
 //:}
 //:```
 
-
-
 //: ## Step 10
 //: These loops will allow you to match up every rank with every suit. Make a `Card` object from all these pairings and append each card to the `cards` property of the deck. At the end of the `init` method, the `cards` array should contain a full deck of standard playing card objects.
-
-
-
-
 
 //: ## Step 11
 //: Add a method to the deck called `drawCard()`. It takes no arguments and it returns a `Card` object. Have it draw a random card from the deck of cards and return it.
 //: - Callout(Hint): There should be `52` cards in the deck. So what if you created a random number within those bounds and then retrieved that card from the deck? Remember that arrays are indexed from `0` and take that into account with your random number picking.
 
-
-
-
-
 //: ## Step 12
 //: Create a protocol for a `CardGame`. It should have two requirements:
 //: * a gettable `deck` property
 //: * a `play()` method
-
-
-
-
+protocol CardGame {
+    var deck: Deck { get }
+    
+    func play() {
+    }
+}
 //: ## Step 13
 //: Create a protocol for tracking a card game as a delegate called `CardGameDelegate`. It should have two functional requirements:
 //: * a function called `gameDidStart` that takes a `CardGame` as an argument
 //: * a function with the following signature: `game(player1DidDraw card1: Card, player2DidDraw card2: Card)`
-
-
-
-
+protocol CardGameDelegate {
+    func gameDidStart(game: CardGame)
+    }
+    func game(playerDidDraw card1: Card, player2DidDraw card2: Card) {
+}
 //: ## Step 14
 //: Create a class called `HighLow` that conforms to the `CardGame` protocol. It should have an initialized `Deck` as a property, as well as an optional delegate property of type `CardGameDelegate`.
 
